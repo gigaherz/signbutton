a3802572283ea32b7c89829a4e799e2a66b6b473
gigaherz <gigaherz@gmail.com>
2024-12-29 01:13:05 +0100

    Port to 1.21.4. Move models to datagen.

--- src/main/java/dev/gigaherz/signbutton/ModSignButton.java
+++ src/main/java/dev/gigaherz/signbutton/ModSignButton.java
@@ -1,12 +1,20 @@
 package dev.gigaherz.signbutton;
 
-import com.mojang.logging.LogUtils;
 import dev.gigaherz.signbutton.button.SignButtonBlock;
 import dev.gigaherz.signbutton.button.SignButtonBlockEntity;
 import dev.gigaherz.signbutton.button.SignButtonItem;
 import dev.gigaherz.signbutton.button.SignButtonWoodTypes;
 import dev.gigaherz.signbutton.client.ClientUtils;
 import dev.gigaherz.signbutton.network.OpenSignButtonEditor;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.client.data.models.blockstates.Variant;
+import net.minecraft.client.data.models.blockstates.VariantProperties;
+import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelLocationUtils;
+import net.minecraft.client.renderer.item.ClientItem;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -17,6 +25,7 @@ import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.recipes.*;
 import net.minecraft.data.recipes.packs.VanillaRecipeProvider;
 import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
@@ -25,7 +34,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
@@ -33,7 +41,6 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
@@ -42,7 +49,6 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import org.slf4j.Logger;
 
 import java.util.List;
 import java.util.Map;
@@ -54,8 +60,6 @@ import java.util.stream.Collectors;
 @Mod(ModSignButton.MODID)
 public class ModSignButton
 {
-    private static final Logger logger = LogUtils.getLogger();
-
     public static final String MODID = "signbutton";
 
     private static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
@@ -87,7 +91,7 @@ public class ModSignButton
     public static DeferredItem<SignButtonItem> WARPED_SIGN_BUTTON_ITEM = ITEMS.registerItem("warped_sign_button", props -> new SignButtonItem(WARPED_SIGN_BUTTON.get(), props.stacksTo(16).useBlockDescriptionPrefix()));
 
     public static DeferredHolder<BlockEntityType<?>, BlockEntityType<SignButtonBlockEntity>> SIGN_BUTTON_BLOCK_ENTITY = BLOCK_ENTITIES.register("sign_button", () ->
-            new BlockEntityType<>(SignButtonBlockEntity::new,
+            new BlockEntityType<>(SignButtonBlockEntity::new, true,
                     ACACIA_SIGN_BUTTON.get(), BIRCH_SIGN_BUTTON.get(), DARK_OAK_SIGN_BUTTON.get(),
                     JUNGLE_SIGN_BUTTON.get(), OAK_SIGN_BUTTON.get(), SPRUCE_SIGN_BUTTON.get(),
                     MANGROVE_SIGN_BUTTON.get(), BAMBOO_SIGN_BUTTON.get(), CHERRY_SIGN_BUTTON.get(),
@@ -135,22 +139,64 @@ public class ModSignButton
         registrar.playToClient(OpenSignButtonEditor.TYPE, OpenSignButtonEditor.CODEC, OpenSignButtonEditor::handle);
     }
 
-    private void gatherData(GatherDataEvent event)
+    private void gatherData(GatherDataEvent.Client event)
     {
         Datagen.gatherData(event);
     }
 
     private static class Datagen
     {
-        public static void gatherData(GatherDataEvent event)
+        public static void gatherData(GatherDataEvent.Client event)
         {
             DataGenerator gen = event.getGenerator();
 
-            gen.addProvider(event.includeServer(), new Recipes(gen.getPackOutput(), event.getLookupProvider()));
-            gen.addProvider(event.includeServer(), Loot.create(gen.getPackOutput(), event.getLookupProvider()));
-            gen.addProvider(event.includeServer(), new BlockTags(gen.getPackOutput(), event.getExistingFileHelper(), event.getLookupProvider()));
-            gen.addProvider(event.includeServer(), new ItemTags(gen.getPackOutput(), event.getExistingFileHelper(), event.getLookupProvider()));
+            gen.addProvider(true, new ModelsAndClientItems(gen.getPackOutput()));
+            gen.addProvider(true, new Recipes(gen.getPackOutput(), event.getLookupProvider()));
+            gen.addProvider(true, Loot.create(gen.getPackOutput(), event.getLookupProvider()));
+            gen.addProvider(true, new BlockTags(gen.getPackOutput(), event.getLookupProvider()));
+            gen.addProvider(true, new ItemTags(gen.getPackOutput(), event.getLookupProvider()));
         }
+
+        private static class ModelsAndClientItems extends ModelProvider
+        {
+            public ModelsAndClientItems(PackOutput output)
+            {
+                super(output, MODID);
+            }
+
+            @Override
+            protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels)
+            {
+                generateForType(OAK_SIGN_BUTTON, Blocks.OAK_PLANKS, blockModels, OAK_SIGN_BUTTON_ITEM, Items.OAK_SIGN, itemModels);
+                generateForType(SPRUCE_SIGN_BUTTON, Blocks.SPRUCE_PLANKS, blockModels, SPRUCE_SIGN_BUTTON_ITEM, Items.SPRUCE_SIGN, itemModels);
+                generateForType(BIRCH_SIGN_BUTTON, Blocks.BIRCH_PLANKS, blockModels, BIRCH_SIGN_BUTTON_ITEM, Items.BIRCH_SIGN, itemModels);
+                generateForType(JUNGLE_SIGN_BUTTON, Blocks.JUNGLE_PLANKS, blockModels, JUNGLE_SIGN_BUTTON_ITEM, Items.JUNGLE_SIGN, itemModels);
+                generateForType(ACACIA_SIGN_BUTTON, Blocks.ACACIA_PLANKS, blockModels, ACACIA_SIGN_BUTTON_ITEM, Items.ACACIA_SIGN, itemModels);
+                generateForType(DARK_OAK_SIGN_BUTTON, Blocks.DARK_OAK_PLANKS, blockModels, DARK_OAK_SIGN_BUTTON_ITEM, Items.DARK_OAK_SIGN, itemModels);
+                generateForType(MANGROVE_SIGN_BUTTON, Blocks.MANGROVE_PLANKS, blockModels, MANGROVE_SIGN_BUTTON_ITEM, Items.MANGROVE_SIGN, itemModels);
+                generateForType(BAMBOO_SIGN_BUTTON, Blocks.BAMBOO_PLANKS, blockModels, BAMBOO_SIGN_BUTTON_ITEM, Items.BAMBOO_SIGN, itemModels);
+                generateForType(CHERRY_SIGN_BUTTON, Blocks.CHERRY_PLANKS, blockModels, CHERRY_SIGN_BUTTON_ITEM, Items.CHERRY_SIGN, itemModels);
+                generateForType(CRIMSON_SIGN_BUTTON, Blocks.CRIMSON_PLANKS, blockModels, CRIMSON_SIGN_BUTTON_ITEM, Items.CRIMSON_SIGN, itemModels);
+                generateForType(WARPED_SIGN_BUTTON, Blocks.WARPED_PLANKS, blockModels, WARPED_SIGN_BUTTON_ITEM, Items.WARPED_SIGN, itemModels);
+            }
+
+            private void generateForType(DeferredBlock<SignButtonBlock> block, Block blockParticlesFrom, BlockModelGenerators blockModels,
+                                         DeferredItem<SignButtonItem> item, Item itemTextureFrom, ItemModelGenerators itemModels)
+            {
+                var blockModel = blockModels.createParticleOnlyBlockModel(block.get(), blockParticlesFrom);
+
+                blockModels.blockStateOutput.accept(MultiVariantGenerator.multiVariant(block.get(),
+                        Variant.variant().with(VariantProperties.MODEL, blockModel)));
+
+                itemModels.generateLayeredItem(item.get(),
+                        ModelLocationUtils.getModelLocation(itemTextureFrom),
+                        ResourceLocation.fromNamespaceAndPath("signbutton", "item/sign_button"));
+
+                itemModels.itemModelOutput.accept(item.get(),
+                        ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(item.get())));
+            }
+        }
+
 
         private static class Recipes extends RecipeProvider.Runner
         {
@@ -246,10 +292,10 @@ public class ModSignButton
 
         private static class BlockTags extends IntrinsicHolderTagsProvider<Block>
         {
-            public BlockTags(PackOutput packOutput, ExistingFileHelper existingFileHelper, CompletableFuture<HolderLookup.Provider> lookup)
+            public BlockTags(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookup)
             {
                 super(packOutput, Registries.BLOCK, lookup,
-                        (block) -> BuiltInRegistries.BLOCK.getResourceKey(block).orElseThrow(), MODID, existingFileHelper);
+                        (block) -> BuiltInRegistries.BLOCK.getResourceKey(block).orElseThrow(), MODID);
             }
 
             @Override
@@ -273,10 +319,10 @@ public class ModSignButton
 
         private static class ItemTags extends IntrinsicHolderTagsProvider<Item>
         {
-            public ItemTags(PackOutput packOutput, ExistingFileHelper existingFileHelper, CompletableFuture<HolderLookup.Provider> lookup)
+            public ItemTags(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookup)
             {
                 super(packOutput, Registries.ITEM, lookup,
-                        (item) -> BuiltInRegistries.ITEM.getResourceKey(item).orElseThrow(), MODID, existingFileHelper);
+                        (item) -> BuiltInRegistries.ITEM.getResourceKey(item).orElseThrow(), MODID);
             }
 
             @Override
